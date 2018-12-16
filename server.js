2a617ffda989f6857b3103d7079867f596fd6b99
Andrew <nana9595@yandex.ru>
2018-12-17 01:31:26 +0700

    fix server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require(`fs`);
 const app = express();
 const jsonParser = bodyParser.json();
 
-app.post(`/api/poll-form/info`, bodyParser, (req, res) => {
+app.post(`/api/poll-form/info`, jsonParser, (req, res) => {
 	res.send(req.body);
 });
 
