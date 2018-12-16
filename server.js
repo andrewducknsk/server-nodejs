9fd9f27122955592483e93166550b20dc092359a
Andrew <nana9595@yandex.ru>
2018-12-17 01:29:44 +0700

    created post request

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const fs = require(`fs`);
+
+const app = express();
+const jsonParser = bodyParser.json();
+
+app.post(`/api/poll-form/info`, bodyParser, (req, res) => {
+	res.send(req.body);
+});
+
+app.listen(3000);
