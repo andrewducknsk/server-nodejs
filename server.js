d6f067b59fc17f8fdefa3df9c8c3e94ecb99779b
Andrew <nana9595@yandex.ru>
2018-12-17 01:45:42 +0700

    added set response headers

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const fs = require(`fs`);
 const app = express();
 const jsonParser = bodyParser.json();
 
+app.use((req, res, next) => {
+	res.setHeader(`Access-Control-Allow-Origin`, `*`);
+	res.setHeader(
+		`Access-Control-Allow-Methods`,
+		`GET, POST, PUT, DELETE, OPTIONS`
+	);
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With,content-type'
+	);
+	res.setHeader('Access-Control-Allow-Credentials', false);
+	next();
+});
+
 app.post(`/api/poll-form/info`, jsonParser, (req, res) => {
 	res.send(req.body);
 });
